30ec2d5b91e3b6aa72ccee948ce6119476f9774d
Leo Borquez <ele.dev@protonmail.com>
2019-09-18 22:39:50 -0400

    Symbol form validations

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,9 +1,19 @@
 class StocksController < ApplicationController
 
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
-    #render json: @stock
-    render 'users/portafolio'
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock
+        #render json: @stock
+        render 'users/portafolio'
+      else
+        flash[:danger] = "That symbol doesn't exist"
+        redirect_to portafolio_path
+      end
+    else
+      flash[:danger] = "You need the company Symbol"
+      redirect_to portafolio_path
+    end
   end
 
 end
