f6cd1a815e6bc288882e0ee4396c14258a10064b
Leo Borquez <ele.dev@protonmail.com>
2019-09-20 16:10:57 -0400

    js file for the result

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,7 +5,9 @@ class StocksController < ApplicationController
       @stock = Stock.new_from_lookup(params[:stock])
       if @stock
         #render json: @stock
-        render partial: 'users/result' #render only the partial
+        respond_to do |format|
+          format.js { render partial: 'users/result' }#render only the partial
+        end
       else
         flash[:danger] = "That symbol doesn't exist"
         redirect_to portafolio_path
