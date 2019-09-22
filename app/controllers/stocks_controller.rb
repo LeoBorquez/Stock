91049ca2d1e4f0f1442c42e15052db394b3325ca
Leo Borquez <ele.dev@protonmail.com>
2019-09-21 22:21:53 -0400

    Readme

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,7 +2,7 @@ class StocksController < ApplicationController
 
   def search
     if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
+      @stock = Stock.new_from_lookup(params[:stock])rvm use
       if @stock
         #render json: @stock
         respond_to do |format|
