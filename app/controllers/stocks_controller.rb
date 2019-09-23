11db5e04aae6c78065ae23ddf978193213f7090a
Leo Borquez <ele.dev@protonmail.com>
2019-09-23 17:35:23 -0400

    refactor controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,25 +1,15 @@
 class StocksController < ApplicationController
 
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        #render json: @stock
-        respond_to do |format|
-          format.js { render partial: 'users/result' } #render only the partial
-        end
-      else
-        respond_to do |format|
-          flash.now[:danger] = "That symbol doesn't exist"
-          format.js { render partial: 'users/result' }
-        end
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = "You need the company Symbol"
     else
-      respond_to do |format|
-        flash.now[:danger] = "You need the company Symbol"
-        format.js { render partial: 'users/result' }
-      end
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "That symbol doesn't exist" unless @stock
+    end
+    respond_to do |format|
+      format.js { render partial: 'users/result' }
     end
   end
-
 end
+
