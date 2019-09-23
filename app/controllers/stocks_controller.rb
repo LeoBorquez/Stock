8783ff67a370c0011ae4c3ac925f2381c29d3bbd
Leo Borquez <ele.dev@protonmail.com>
2019-09-22 21:09:49 -0400

    fix ajax error messages

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,19 +2,23 @@ class StocksController < ApplicationController
 
   def search
     if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])rvm use
+      @stock = Stock.new_from_lookup(params[:stock])
       if @stock
         #render json: @stock
         respond_to do |format|
-          format.js { render partial: 'users/result' }#render only the partial
+          format.js { render partial: 'users/result' } #render only the partial
         end
       else
-        flash[:danger] = "That symbol doesn't exist"
-        redirect_to portafolio_path
+        respond_to do |format|
+          flash.now[:danger] = "That symbol doesn't exist"
+          format.js { render partial: 'users/result' }
+        end
       end
     else
-      flash[:danger] = "You need the company Symbol"
-      redirect_to portafolio_path
+      respond_to do |format|
+        flash.now[:danger] = "You need the company Symbol"
+        format.js { render partial: 'users/result' }
+      end
     end
   end
 
