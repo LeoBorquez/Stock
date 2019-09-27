d29a100b64268372a39fed1c129c76aefe8f3c38
Leo Borquez <ele.dev@protonmail.com>
2019-09-26 23:57:25 -0400

    User registration controller
    Custom controller inherits everything from Devise class, if something
    isn't here rails will go to Devise.

---
+++ app/controllers/user/registrations_controller.rb
@@ -0,0 +1,12 @@
+class User::RegistrationsController < Devise::RegistrationsController
+
+  before_action :configure_permitted_parameters
+
+  protected
+
+  def configure_permitted_parameters
+
+  end
+
+
+end
